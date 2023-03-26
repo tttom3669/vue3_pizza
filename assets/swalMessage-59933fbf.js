db98e59dbfcf83c39c9b4169b15380290b2daaad
tttom3669 <tttom3669@gmail.com>
2023-03-26 14:08:10 +0800

    deploy

---
+++ assets/swalMessage-59933fbf.js
@@ -0,0 +1,1 @@
+import{k as a}from"./index-138d938a.js";import{S as t}from"./sweetalert2.all-07293f25.js";const f=a("swalMessage",{actions:{swalShow(s,e,o="message"){o==="message"?t.fire({icon:`${e}`,title:`${s}`}):o==="toast"&&t.fire({toast:!0,position:"bottom-end",showConfirmButton:!1,icon:`${e}`,timer:3e3,title:`${s}`})}}});export{f as s};

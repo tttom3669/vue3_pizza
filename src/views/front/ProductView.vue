6c4f8011f97c8c2c2dcb9c37c8c1cc30efd0c254
tttom3669 <tttom3669@gmail.com>
2023-04-04 20:27:44 +0800

    update: product detail

--- src/views/front/ProductView.vue
+++ src/views/front/ProductView.vue
@@ -22,12 +22,47 @@
       </nav>
       <!-- 產品 -->
       <div class="row mt-5">
-        <div class="col-lg-4">
+        <div class="col-lg-4 position-relative">
           <img
             :src="tempProduct.imageUrl"
             :alt="tempProduct.title"
             class="w-100 object-fit-cover img-fluid"
           />
+          <div
+            class="me-4 position-absolute translate-middle"
+            type="button"
+            v-if="collectionList?.indexOf(tempProduct.id) === -1"
+            @click="() => updateCollection(tempProduct)"
+            style="top: 30px; left: 30px"
+          >
+            <i
+              class="bi collection"
+              :class="{
+                'bi-heart': collectionList?.indexOf(tempProduct.id) === -1,
+                'bi-suit-heart-fill':
+                  collection.hover == true &&
+                  collection.itemID == tempProduct.id,
+              }"
+              @mouseover="
+                () => (
+                  (collection.hover = true),
+                  (collection.itemID = tempProduct.id)
+                )
+              "
+              @mouseout="
+                () => ((collection.hover = false), (collection.itemID = ''))
+              "
+            ></i>
+          </div>
+          <div
+            class="me-4 position-absolute translate-middle"
+            type="button"
+            v-else
+            @click="() => updateCollection(tempProduct)"
+            style="top: 30px; left: 30px"
+          >
+            <i class="bi bi-suit-heart-fill" style="color: red"></i>
+          </div>
         </div>
         <div class="col-lg-8">
           <div class="d-flex flex-row align-items-baseline">
@@ -104,7 +139,7 @@
               <div class="col-6">
                 <button
                   type="button"
-                  class="btn btn-primary  w-100 text-white"
+                  class="btn btn-primary w-100 text-white"
                   @click.prevent="() => addToCart_Check()"
                   :disabled="loadingItem === tempProduct.id"
                 >
@@ -126,19 +161,97 @@
           </div>
         </div>
       </div>
-      <!-- 相關商品 -->
-      <div
-        class="d-flex flex-column align-items-start justify-content-center mt-3"
-      >
-        <h3 class="fw-bold mt-1 border-3 border-start border-primary">
-          <span class="text-cusDarkBrown ms-3">精選商品</span>
-        </h3>
+      <!-- 注意事項-->
+      <nav>
+        <div class="nav nav-pills mt-5" id="nav-tab" role="tablist">
+          <div class="d-flex flex-row bg-white">
+            <button
+              class="nav-link active"
+              id="nav-home-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-home"
+              type="button"
+              role="tab"
+              aria-controls="nav-home"
+              aria-selected="true"
+            >
+              商品介紹
+            </button>
+            <button
+              class="nav-link nav-button"
+              id="nav-profile-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-profile"
+              type="button"
+              role="tab"
+              aria-controls="nav-profile"
+              aria-selected="false"
+            >
+              訂購須知
+            </button>
+          </div>
+        </div>
+      </nav>
+      <div class="tab-content" id="nav-tabContent">
+        <div
+          class="tab-pane fade show active p-3 bg-white border"
+          id="nav-home"
+          role="tabpanel"
+          aria-labelledby="nav-home-tab"
+        >
+          <ul class="list-unstyled text-cusBrown">
+            <li class="fw-bold text-cusDarkBrown">
+              品名︰{{ tempProduct.title }}
+              <span v-if="tempProduct.category === '披薩'">12吋</span>
+            </li>
+            <li>成分︰{{ tempProduct.content }}</li>
+            <li>保存方式：請盡快食用完畢。</li>
+            <li v-if="tempProduct.category === '披薩'">
+              過敏原資訊︰本產品含有麩質、奶類，不適合對其過敏體質者食用。
+            </li>
+          </ul>
+        </div>
+        <div
+          class="tab-pane fade p-3 bg-white border"
+          id="nav-profile"
+          role="tabpanel"
+          aria-labelledby="nav-profile-tab"
+        >
+          <div class="row">
+            <div class="col-md-6">
+              <h6 class="fw-bold text-cusDarkBrown">運送服務：</h6>
+              <ul class="list-unstyled text-cusBrown">
+                <li>
+                  提供外送區域為桃園市！所有品項滿 $399 免運，未滿$399，運費$70
+                </li>
+                <li>收到訂單及款項後，預計1小時內出貨。</li>
+              </ul>
+            </div>
+            <div class="col-md-6">
+              <h6 class="fw-bold text-cusDarkBrown">取消訂單：</h6>
+              <ul class="list-unstyled text-cusBrown">
+                <li>
+                  在該筆訂單尚未製作之前皆可。請消費者以電話方式聯絡門市，由門市端取消訂單。
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-      <SwiperTopFlavors :category="'全部商品'" />
+      <!-- 相關商品 -->
+      <FrontHeading class="mt-5" :title="'精選商品'" />
+      <SwiperTopFlavors class="mb-3" :category="'全部商品'" />
     </div>
   </div>
   <VueLoading v-model:active="isLoading" :loader="'dots'" />
 </template>
+
+<style>
+.nav-button .active {
+  background-color: black;
+}
+</style>
+
 <script>
 import { mapState, mapActions } from 'pinia';
 import productsStore from '@/stores/productsStore';
@@ -147,6 +260,8 @@ import loadingStore from '@/stores/loadingStore';
 import swalMessage from '@/stores/swalMessage';
 import SwiperTopFlavors from '@/components/front/SwiperTopFlavors.vue';
 import { RouterLink } from 'vue-router';
+import FrontHeading from '@/components/front/FrontHeading.vue';
+import collectionStore from '@/stores/collectionStore';
 
 export default {
   data() {
@@ -155,14 +270,16 @@ export default {
       productId: '',
     };
   },
-  components: { SwiperTopFlavors, RouterLink },
+  components: { SwiperTopFlavors, RouterLink, FrontHeading },
   props: ['id'],
   computed: {
     ...mapState(cartStore, ['cart']),
     ...mapState(loadingStore, ['isLoading', 'loadingItem']),
     ...mapState(productsStore, ['tempProduct']),
+    ...mapState(collectionStore, ['collectionList', 'collection']),
   },
   methods: {
+    ...mapActions(collectionStore, ['getCollection', 'updateCollection']),
     ...mapActions(swalMessage, ['swalShow']),
     ...mapActions(cartStore, ['addToCart', 'updateCartItem']),
     ...mapActions(productsStore, ['getProduct', 'changeCategory']),
@@ -182,6 +299,7 @@ export default {
   },
   mounted() {
     this.getProduct(this.id);
+    this.getCollection();
   },
 };
 </script>
